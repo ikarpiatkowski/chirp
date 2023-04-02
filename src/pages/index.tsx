e4e0bc406139f6bec3fdb2ff0d2f27da625f666c
Ikar <ikarpiatkowski@proton.me>
2023-04-02 03:14:25 +0200

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  const { data } = api.posts.getAll.useQuery();
   const user = useUser();
-
   return (
     <>
       <Head>
@@ -21,6 +19,11 @@ const Home: NextPage = () => {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
